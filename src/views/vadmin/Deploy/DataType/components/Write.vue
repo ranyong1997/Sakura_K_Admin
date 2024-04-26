169fe87eb550e4d9508113aa30f12762a758bd36
luck_baby <ranyong1209@gmail.com>
2024-04-26 22:09:06 +0800

    ✨ Feat(): 新增数据类型页面

---
+++ src/views/vadmin/Deploy/DataType/components/Write.vue
@@ -0,0 +1,159 @@
+<script setup lang="tsx">
+import { Form, FormSchema } from '@/components/Form'
+import { useForm } from '@/hooks/web/useForm'
+import { PropType, reactive, watch } from 'vue'
+import { useValidator } from '@/hooks/web/useValidator'
+import { getDataTypeListApi } from '@/api/vadmin/deploy/data'
+const { required } = useValidator()
+const props = defineProps({
+  currentRow: {
+    type: Object as PropType<any>,
+    default: () => null
+  }
+})
+const formSchema = reactive<FormSchema[]>([
+  {
+    field: 'data_name',
+    label: '数据源名称',
+    component: 'Input',
+    colProps: {
+      span: 24
+    },
+    componentProps: {
+      style: {
+        width: '100%'
+      },
+      maxlength: 10,
+      showWordLimit: true
+    }
+  },
+  {
+    field: 'type',
+    label: '类型',
+    colProps: {
+      span: 24
+    },
+    component: 'Select',
+    componentProps: {
+      style: {
+        width: '100%'
+      },
+      props: {
+        label: 'type_name',
+        value: 'type_name'
+      },
+      showWordLimit: true
+    },
+    // 下拉选择数据源类型
+    optionApi: async () => {
+      const res = await getDataTypeListApi()
+      return res.data
+    }
+
+  },
+  {
+    field: 'host',
+    label: '地址',
+    component: 'Input',
+    colProps: {
+      span: 24
+    },
+    componentProps: {
+      style: {
+        width: '100%'
+      },
+      maxlength: 20,
+      showWordLimit: true,
+      placeholder: "127.0.0.1"
+    }
+  },
+  {
+    field: 'port',
+    label: '端口',
+    component: 'Input',
+    colProps: {
+      span: 24
+    },
+    componentProps: {
+      style: {
+        width: '100%'
+      },
+      maxlength: 10,
+      showWordLimit: true,
+      placeholder: "3306"
+    },
+
+  },
+  {
+    field: 'username',
+    label: '用户名',
+    component: 'Input',
+    colProps: {
+      span: 24
+    },
+    componentProps: {
+      style: {
+        width: '100%'
+      },
+      maxlength: 10,
+      showWordLimit: true,
+      placeholder: "root"
+    }
+  },
+  {
+    field: 'password',
+    label: '密码',
+    component: 'Input',
+    colProps: {
+      span: 24
+    },
+    componentProps: {
+      style: {
+        width: '100%'
+      },
+      maxlength: 20,
+      type: "password",
+      showPassword: true,
+      showWordLimit: true
+    }
+  }
+])
+
+const rules = reactive({
+  data_name: [required()],
+  type: [required()],
+  host: [required()],
+  port: [required()],
+  username: [required()],
+  password: [required()]
+
+})
+const { formRegister, formMethods } = useForm()
+const { setValues, getFormData, getElFormExpose } = formMethods
+const submit = async () => {
+  const elForm = await getElFormExpose()
+  const valid = await elForm?.validate()
+  if (valid) {
+    const formData = await getFormData()
+    return formData
+  }
+}
+watch(
+  () => props.currentRow,
+  (currentRow) => {
+    if (!currentRow) return
+    setValues(currentRow)
+  },
+  {
+    deep: true,
+    immediate: true
+  }
+)
+defineExpose({
+  submit
+})
+</script>
+
+<template>
+  <Form :rules="rules" @register="formRegister" :schema="formSchema" />
+</template>
