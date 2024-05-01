34a5982b8437689e2d328e20d1ad6ba57c04b2e0
ranyong <ranyong1209@gmail.com>
2024-05-01 11:06:22 +0800

    🎈 perf(Optimize page display): 优化页面显示

--- src/views/vadmin/Tools/QueryDB/QueryDB.vue
+++ src/views/vadmin/Tools/QueryDB/QueryDB.vue
@@ -1,11 +1,17 @@
 <script setup lang="tsx">
 import { Form, FormSchema } from '@/components/Form'
 import { reactive, ref, unref } from 'vue'
-import { ElContainer, ElAside, ElMain, ElHeader, ElTree } from 'element-plus'
+import { ElContainer, ElAside, ElMain, ElHeader, ElTree, ElButton } from 'element-plus'
 import type Node from 'element-plus/es/components/tree/src/model/node'
 import { ContentWrap } from '@/components/ContentWrap'
 import { getDbListApi, getTableListApi, mysqlExecuteApi } from '@/api/vadmin/tools/querydb'
 import { getDataSourceListApi } from '@/api/vadmin/deploy/data'
+import { BaseButton } from '@/components/Button'
+
+const dialogVisible = ref(false)
+const dialogTitle = ref('')
+const currentRow = ref({})
+const actionType = ref('')
 
 // 获取数据
 const getLists = async (data: any) => {
@@ -23,6 +29,18 @@ const getLists = async (data: any) => {
         total: res.count || 0
     }
 }
+
+// 编辑方法
+const editAction = async (row: any) => {
+    const res = await getProjectListApi(row.id)
+    if (res) {
+        dialogTitle.value = '编辑项目'
+        actionType.value = 'edit'
+        currentRow.value = row
+        dialogVisible.value = true
+    }
+}
+const currentDb = ref(''); // 使用ref
 let selectOptions = ref([])
 const formSchema = reactive<FormSchema[]>([
     {
@@ -37,7 +55,7 @@ const formSchema = reactive<FormSchema[]>([
         reserveKeyword: true,
         componentProps: {
             style: {
-                width: '10%'
+                width: '200px'
             },
             props: {
                 label: 'data_name',
@@ -45,19 +63,40 @@ const formSchema = reactive<FormSchema[]>([
             },
             on: {
                 change: (e) => {
-                    let res = selectOptions.value.find(item => item.id == e);
+                    const res = selectOptions.value.find(item => item.id == e);
                     console.log(res);
                 }
-            },
+            }
         },
-        // 下拉选择用户
+        // 下拉选择数据源
         optionApi: async () => {
             const res = await getDataSourceListApi()
             selectOptions.value = res.data;
             return res.data
         }
+    },
+    {
+        slots: {
+            default: (data: any) => {
+                const row = data.row
+                const add = ['auth.user.cereate'] // 编辑权限控制
+                return (
+                    <>
+                        <BaseButton
+                            size="small"
+                            v-hasPermi={add} // 检查更新权限
+                            onClick={() => addSource(row)}
+                        >
+                            新增
+                        </BaseButton>
+                    </>
+                )
+            }
+        }
     }
 ])
+
+
 const searchParams = ref({})
 interface Tree {
     name: string
@@ -92,9 +131,20 @@ const loadNode = (node: Node, resolve: (data: Tree[]) => void) => {
 </script>
 
 <template>
-    <ContentWrap>
-        <Form :schema="formSchema" />
-        <span>当前库:</span>
+    <ContentWrap class="dbTreeForDbQuery">
+        <div class="wrapper-op">
+            <Form :schema="formSchema" />
+            <ElRow :gutter="10">
+                <ElCol :span="1.5" v-hasPermi="['auth.user.create']">
+                    <BaseButton type="primary" @click="addSource">
+                        <Icon icon="icon-park-outline:add-one" />新增
+                    </BaseButton>
+                </ElCol>
+            </ElRow>
+        </div>
+        <div class="tree-current-node">
+            <span>当前库:</span>
+        </div>
         <ElContainer>
             <!-- 左侧 -->
             <ElAside width="400px">
@@ -109,3 +159,38 @@ const loadNode = (node: Node, resolve: (data: Tree[]) => void) => {
         </ElContainer>
     </ContentWrap>
 </template>
+
+
+<style lang="less" scoped>
+.dbTreeForDbQuery {
+    padding: 0 6px;
+    width: 100%;
+    overflow-x: auto;
+    overflow-y: hidden;
+    height: 100%;
+
+    .wrapper-op {
+        min-width: 220px;
+        margin-bottom: 8px;
+        display: flex;
+        align-items: stretch;
+    }
+
+    .tree-current-node {
+        padding: 0 0 8px 6px;
+        border-bottom: 1px solid #dee2ea;
+        margin-bottom: 6px;
+
+        span {
+            color: #2c2f37;
+            font-weight: 600;
+            font-size: 12px
+        }
+    }
+
+    .tree-database-list {
+        height: calc(100% - 70px);
+        overflow-y: auto;
+    }
+}
+</style>
