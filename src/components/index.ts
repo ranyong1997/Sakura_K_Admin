1c320349d0335bcc479bd7844a92120449491e5f
ranyong <ranyong1209@gmail.com>
2024-05-01 15:55:01 +0800

    🎈 perf(optimized code): 优化代码

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,11 +2,11 @@ import type { App } from 'vue'
 import { Icon } from './Icon'
 import { Permission } from './Permission'
 import { BaseButton } from './Button'
-import { Monaco } from './Monaco/index.vue'
+// import { Monaco } from './Monaco/index.vue'
 
 export const setupGlobCom = (app: App<Element>): void => {
   app.component('Icon', Icon)
   app.component('Permission', Permission)
   app.component('BaseButton', BaseButton)
-  app.component('Monaco', Monaco)
+  // app.component('Monaco', Monaco)
 }
