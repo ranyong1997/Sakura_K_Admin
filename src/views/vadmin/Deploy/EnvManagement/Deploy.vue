418647434a7819ab304997e00b101a3eb9eeea8e
luck_baby <ranyong1209@gmail.com>
2024-05-07 10:53:28 +0800

    🎈 Perf(Remove the environment management search function): 移除环境管理搜索功能

--- src/views/vadmin/Deploy/EnvManagement/Deploy.vue
+++ src/views/vadmin/Deploy/EnvManagement/Deploy.vue
@@ -1,13 +1,13 @@
 <script setup lang="tsx">
-import { computed, onMounted, reactive, ref, unref } from 'vue'
+import {computed,onMounted, reactive, ref, unref } from 'vue'
 import { useTable } from '@/hooks/web/useTable'
 import { useI18n } from '@/hooks/web/useI18n'
 import { Table, TableColumn } from '@/components/Table'
-import { ElRow, ElCol, ElMessage } from 'element-plus'
+import { ElRow, ElCol,ElMessage } from 'element-plus'
 import { Search } from '@/components/Search'
 import { ContentWrap } from '@/components/ContentWrap'
 import { Dialog } from '@/components/Dialog'
-import { getEnvListApi, addEnvApi, putEnvApi, delEnvApi } from '@/api/vadmin/deploy/env'
+import { getEnvListApi,addEnvApi,putEnvApi,delEnvApi } from '@/api/vadmin/deploy/env'
 import { useAuthStoreWithOut } from '@/store/modules/auth'
 import { BaseButton } from '@/components/Button'
 import { useDictStore } from '@/store/modules/dict'
@@ -15,7 +15,7 @@ import { selectDictLabel, DictDetail } from '@/utils/dict'
 import Write from './components/Write.vue'
 
 // 获取数据
-const getLists = async (data: any) => {
+const getLists = async (data:any) => {
   const { pageSize, currentPage } = tableState
   const res = await getEnvListApi({
     page: unref(currentPage),
@@ -41,41 +41,24 @@ const { tableRegister, tableState, tableMethods } = useTable({
       ...unref(searchParams),
     })
   },
-  fetchDelApi: async (value) => {
+  fetchDelApi: async (value) =>{
     const res = await delEnvApi(value)
     return res.code === 200
   }
 })
 
-
-const selectDictLabel = (options: Record<string, any>, value: string) => {
-  const option = options[value]
-  console.log("环境名称：label", option.label, "环境名称：value", option.value);
-  return option ? option.label : value
-}
-
-
-const envOptions = ref([])
-const labelToValueMap = ref(new Map())
+const envOptions = ref<DictDetail[]>([])
 
 const getOptions = async () => {
   const dictStore = useDictStore()
   const dictOptions = await dictStore.getDictObj(['env_management'])
   envOptions.value = dictOptions.env_management
-  // 创建反向映射对象
-  const map = new Map()
-  envOptions.value.forEach(option => {
-    map.set(option.label, option.value)
-  })
-  labelToValueMap.value = map
-  console.log("环境名称：labelToValueMap", labelToValueMap.value);
-  
 }
 
 getOptions()
 
 const { dataList, loading, total, pageSize, currentPage } = tableState
-const { getList, delList } = tableMethods
+const { getList, delList} = tableMethods
 const tableColumns = reactive<TableColumn[]>([
   // 多选框
   {
@@ -94,11 +77,7 @@ const tableColumns = reactive<TableColumn[]>([
     field: 'env_name',
     label: '环境名称',
     disabled: true,
-    componentProps: {
-      style: {
-        width: '100%'
-      }
-    },
+    width: '170px',
     slots: {
       default: (data: any) => {
         const row = data.row
@@ -145,10 +124,10 @@ const tableColumns = reactive<TableColumn[]>([
         const del = ['auth.user.delete'] // 删除权限控制
         return (
           <>
-            <BaseButton
-              type="primary"
-              link
-              size="small"
+            <BaseButton 
+              type="primary" 
+              link 
+              size="small" 
               v-hasPermi={update} // 检查更新权限
               onClick={() => editAction(row)}
             >
@@ -178,12 +157,8 @@ const searchSchema = reactive<FormSchema[]>([
     component: 'Input',
     componentProps: {
       clearable: true,
-      style: { width: '214px' },
-      // 在输入时将 label 映射到 value
-      'onUpdate:modelValue': (val: string) => {
-        const value = labelToValueMap.value.get(val) || val
-        // 在这里可以触发搜索或其他操作,使用 `value` 作为实际的搜索值
-        console.log('Searched value:', value)
+      style: {
+        width: '214px'
       }
     },
     formItemProps: {
@@ -196,7 +171,6 @@ const setSearchParams = (data: any) => {
   currentPage.value = 1
   searchParams.value = data
   getList()
-
 }
 
 const dialogVisible = ref(false)
@@ -214,24 +188,24 @@ const addAction = () => {
 }
 // 编辑方法
 const editAction = async (row: any) => {
-  const res = await getEnvListApi(row.id)
-  if (res) {
-    dialogTitle.value = '编辑环境'
-    actionType.value = 'edit'
-    currentRow.value = row
-    dialogVisible.value = true
-  }
+    const res =  await getEnvListApi(row.id)
+    if(res) {
+      dialogTitle.value = '编辑环境'
+      actionType.value = 'edit'
+      currentRow.value = row
+      dialogVisible.value = true
+    }
 }
 // 删除方法
 const delLoading = ref(false)
 
 const delData = async (row?: any) => {
   if (row) {
-    await delList(true, [row.id]).finally(() => {
+    await delList(true, [row.id]).finally(()=>{
       delLoading.value = false
     }) // 删除单个项目
   } else {
-    await delList(true).finally(() => {
+    await delList(true).finally(()=>{
       delLoading.value = false
     }) // 批量删除选中的项目
   }
@@ -239,7 +213,7 @@ const delData = async (row?: any) => {
 }
 
 // 保存方法
-const save = async () => {
+const save = async () =>{
   const write = unref(writeRef)
   const formData = await write?.submit()
   if (formData) {
@@ -254,7 +228,7 @@ const save = async () => {
           getList()
           return ElMessage.success('新增成功')
         }
-      } else if (actionType.value === 'edit') {
+      } else if (actionType.value === 'edit') {        
         res.value = await putEnvApi(formData)
         if (res.value) {
           dialogVisible.value = false
@@ -275,12 +249,23 @@ onMounted(async () => {
 
 <template>
   <ContentWrap>
-    <Search :schema="searchSchema" @reset="setSearchParams" @search="setSearchParams" />
-    <Table v-model:current-page="currentPage" v-model:page-size="pageSize" showAction :columns="tableColumns"
-      default-expand-all node-key="id" :data="dataList" :loading="loading" :pagination="{
+    <!-- <Search :schema="searchSchema" @reset="setSearchParams" @search="setSearchParams" /> -->
+    <Table
+      v-model:current-page="currentPage"
+      v-model:page-size="pageSize"
+      showAction
+      :columns="tableColumns"
+      default-expand-all
+      node-key="id"
+      :data="dataList"
+      :loading="loading"
+      :pagination="{
         total
-      }" @register="tableRegister" @refresh="getList">
-      <template #toolbar>
+      }"
+      @register="tableRegister"
+      @refresh="getList"
+    >
+    <template #toolbar>
         <ElRow :gutter="10">
           <ElCol :span="1.5" v-hasPermi="['auth.user.create']">
             <BaseButton type="primary" @click="addAction">新增环境</BaseButton>
