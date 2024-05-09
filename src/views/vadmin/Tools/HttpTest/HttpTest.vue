4ef35c7dded23199c69c34e763fd0126a29fea13
luck_baby <ranyong1209@gmail.com>
2024-05-09 18:30:24 +0800

    🎈 perf(optimized code): 优化代码

--- src/views/vadmin/Tools/HttpTest/HttpTest.vue
+++ src/views/vadmin/Tools/HttpTest/HttpTest.vue
@@ -1,5 +1,5 @@
 <script setup lang="tsx">
-
+import { ContentWrap } from '@/components/ContentWrap'
 import { ref } from 'vue'
 import { ElInput, ElSelect, ElOption, ElButton } from 'element-plus';
 let input3 = ref('')
@@ -9,18 +9,20 @@ let select = ref('')
 
 
 <template>
-    <el-input v-model="input3" style="max-width: 600px" placeholder="Please input" class="input-with-select">
-        <template #prepend>
-            <el-select v-model="select" placeholder="Select" style="width: 115px">
-                <el-option label="Restaurant" value="1" />
-                <el-option label="Order No." value="2" />
-                <el-option label="Tel" value="3" />
-            </el-select>
-        </template>
-        <template #append>
-            <el-button>btn</el-button>
-        </template>
-    </el-input>
+    <ContentWrap>
+        <el-input v-model="input3" style="max-width: 600px" placeholder="Please input" class="input-with-select">
+            <template #prepend>
+                <el-select v-model="select" placeholder="Select" style="width: 115px">
+                    <el-option label="Restaurant" value="1" />
+                    <el-option label="Order No." value="2" />
+                    <el-option label="Tel" value="3" />
+                </el-select>
+            </template>
+            <template #append>
+                <el-button>btn</el-button>
+            </template>
+        </el-input>
+    </ContentWrap>
 </template>
 
 
